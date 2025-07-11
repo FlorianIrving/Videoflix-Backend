090ae768931815b9fca71bba61888d6d5634283a
IcePain <143926986+BadPain@users.noreply.github.com>
2025-07-11 19:08:36 +0200

    Refactor settings to use environment variables for database configuration and reset ALLOWED_HOSTS for security

--- core/settings.py
+++ core/settings.py
@@ -40,7 +40,7 @@ SECRET_KEY = os.getenv(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = '45.92.216.32', 'localhost', '127.0.0.1', '0.0.0.0'
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -92,31 +92,31 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": os.getenv("DB_HOST"),
-        "PORT": os.getenv("DB_PORT", 5432),
-        "OPTIONS": {
-            "sslmode": "require"
-        }
-    }
-}
-
-
 # DATABASES = {
 #     "default": {
 #         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": os.environ.get("DB_NAME", default="videoflix_db"),
-#         "USER": os.environ.get("DB_USER", default="videoflix_user"),
-#         "PASSWORD": os.environ.get("DB_PASSWORD", default="supersecretpassword"),
-#         "HOST": os.environ.get("DB_HOST", default="db"),
-#         "PORT": os.environ.get("DB_PORT", default=5432)
+#         "NAME": os.getenv("DB_NAME"),
+#         "USER": os.getenv("DB_USER"),
+#         "PASSWORD": os.getenv("DB_PASSWORD"),
+#         "HOST": os.getenv("DB_HOST"),
+#         "PORT": os.getenv("DB_PORT", 5432),
+#         "OPTIONS": {
+#             "sslmode": "require"
+#         }
 #     }
 # }
+
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("DB_NAME", default="videoflix_db"),
+        "USER": os.environ.get("DB_USER", default="videoflix_user"),
+        "PASSWORD": os.environ.get("DB_PASSWORD", default="supersecretpassword"),
+        "HOST": os.environ.get("DB_HOST", default="db"),
+        "PORT": os.environ.get("DB_PORT", default=5432)
+    }
+}
 
 CACHES = {
     "default": {
