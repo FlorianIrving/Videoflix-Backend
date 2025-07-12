ef7a517199a36d1dffba09a1a206661d5b103417
IcePain <143926986+BadPain@users.noreply.github.com>
2025-07-12 22:43:02 +0200

    Remove unused RegisterView import from URL configuration

--- core/urls.py
+++ core/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from auth_app.api.views import RegisterView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
